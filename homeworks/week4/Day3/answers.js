c6e40204e4009e92cd019eec59fad730ac3c4481
RyanTF313 <r.tillman.french@gmail.com>
2023-07-28 17:34:26 -0400

    Fizz Buzz answered

--- homeworks/week4/Day3/answers.js
+++ homeworks/week4/Day3/answers.js
@@ -23,14 +23,36 @@ for (let i = 0; i <= 200; i++) {
   }
 }
 // Second Option
-for (let i = 0; i <= 200; i+=2) {
-    console.log(i);
+for (let i = 0; i <= 200; i += 2) {
+  console.log(i);
 }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+// This is a classic problem that you should get really comfortable solving. If you've solved it before, try to make it more elegant and short.
 
+// Write a javascript application that logs all numbers from 1 - 100.
+// If a number is divisible by 3 log "Fizz" instead of the number.
+// If a number is divisible by 5 log "Buzz" instead of the number.
+// If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+// First Option
+for (let i = 1; i <= 100; i++) {
+  if (i & (3 === 0) && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i & (3 === 0)) {
+    console.log("Fizz");
+  } else if (i & (5 === 0)) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
+// Second Option
+for (let i = 1; i <= 100; i++) {
+  console.log(i % 3 === 0 ? "Fizz" : null + (i % 5) === 0 ? "Buzz" : null || i);
+}
 ////////////////////////////////
 // Wild Wild Life
 ////////////////////////////////
